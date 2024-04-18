234b12abb57f983e45bde8f968ff519edd30a421
Faisal Shahzad <faisalshahzad.ciit@gmail.com>
2024-04-19 00:13:36 +0500

    Initial project with MVVM design pattern

---
+++ app/src/main/java/com/cr/drink_suggester/MainActivity.java
@@ -0,0 +1,54 @@
+package com.cr.drink_suggester;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvDrinkName;
+    ProgressBar progressBar;
+    Button bGetDrink;
+
+    MainViewModel mViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvDrinkName = findViewById(R.id.tvDrinkName);
+        progressBar = findViewById(R.id.progressBar);
+        bGetDrink = findViewById(R.id.bGetDrink);
+
+        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+        mViewModel.getProgress().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer visibility) {
+                progressBar.setVisibility(visibility);
+            }
+        });
+
+        mViewModel.getDrink().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String drinkName) {
+                tvDrinkName.setText(drinkName);
+            }
+        });
+
+        bGetDrink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewModel.suggestNewDrink();
+            }
+        });
+    }
+
+
+}
